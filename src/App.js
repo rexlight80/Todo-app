c22fc4599aabf7f438e7a90c04e98fa47f218d00
Your Name <you@example.com>
2020-11-23 00:14:52 +0530

    todoapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useReducer}from "react"
+import {Col, Container} from "reactstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import { TodoContext } from "./context/TodoContext"
+import todoReducer from "./context/reducer"
+import TodoForm from "./Components/TodoForm"
+import Todos from "./Components/Todos"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App=()=>{
+
+  const [todos,dispatch]=useReducer(todoReducer,[]);
+return(
+<TodoContext.Provider value={{todos,dispatch}}>
+<div className="container">
+
+
+
+
+
+
+<div className="row">
+<div className="col-12">
+<TodoForm/>
+<Todos/>
+</div>
+</div>
+</div>
+
+</TodoContext.Provider>
+
+
+)
 }
 
-export default App;
+export default App
---
+++ src/Components/TodoForm.js
@@ -0,0 +1,53 @@
+import React , {useState,useContext} from "react"
+import {FormGroup,Input,Button,Form,InputGroup,InputGroupAddon} from "reactstrap"
+import { v4  } from 'uuid';
+import { ADD_TODO, REMOVE_TODO } from "../context/action.types";
+import { TodoContext } from "../context/TodoContext";
+
+
+
+const TodoForm=()=>{
+
+    const [todoString,setTodoString]=useState("")
+    const {dispatch}=useContext(TodoContext)
+
+    const handleSubmit=e=>{
+        e.preventDefault()
+        if(todoString===""){
+              return alert("please enter todo")
+        }
+
+        const todo={
+            todoString,
+            id:v4()
+        }
+        dispatch({
+            type:ADD_TODO,
+            payload:todo
+        });
+        
+        setTodoString("");
+    }
+ return( <Form onSubmit={handleSubmit}>
+  <FormGroup>
+  <InputGroup>
+  <Input 
+  type="text"
+  name="todo"
+  id="todo"
+  placeholder="Your next Todo"
+  value={todoString}
+  onChange={e=>setTodoString(e.target.value)}
+
+  />
+  <InputGroupAddon addonType="prepend">
+  <Button color="warning" style={{borderRadius:"5px"}}>Add</Button>
+  </InputGroupAddon>
+  </InputGroup>
+  </FormGroup>
+  </Form>
+ )
+}
+
+
+export default TodoForm
